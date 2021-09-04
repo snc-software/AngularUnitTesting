e16f31d765b04d9875a2716715d28c9398e9e1a4
Scott Crowther <scottcrowther@hotmail.com>
2021-09-04 19:47:05 +0100

    http tests

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from "@angular/core/testing"
+import { inject,TestBed } from "@angular/core/testing"
 import { HeroService } from "./hero.service"
 import { MessageService } from "./message.service";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing"
@@ -7,17 +7,33 @@ describe('HeroService', () => {
 
     let _mockMessageService;
     let _httpTestingController : HttpTestingController;
+    let _service: HeroService;
 
     beforeEach(() => {
         _mockMessageService = jasmine.createSpyObj(['add']);
 
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
-            providers: [HeroService, 
+            providers: [
+                HeroService, 
             {provide: MessageService, useValue: _mockMessageService}]
         });
 
         _httpTestingController = TestBed
             .inject(HttpTestingController);
+        _service = TestBed.inject(HeroService);
+    })
+
+    describe('getHero', () => {
+        it('should call get with the correct url', () => {
+            _service.getHero(4).subscribe();
+
+            const request = _httpTestingController
+                .expectOne('api/heroes/4');
+            request.flush({id:4, name:'SuperDude', strength:100});
+            
+            _httpTestingController.verify();
+            expect(request.request.method).toBe('GET');
+        })
     })
 })
